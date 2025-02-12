30c76ee48990e9d493b5bf1c6f927f6bab826ada
fabiodagostino27 <fabiodago27@gmail.com>
2025-02-12 13:05:12 +0100

    ADD: show route

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,4 +2,20 @@ const posts = require("../data/postsData");
 
 const index = (req, res) => res.json(posts);
 
-module.exports = {index};
+const show = (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find(post => post.id === id);
+    
+    if (!post) {
+        res.status(404);
+
+        return res.json ({
+            error: "Not Found",
+            message: "Post non trovato"
+        })
+    };
+
+    res.json(post);
+}
+
+module.exports = {index, show};
--- routers/posts.js
+++ routers/posts.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {index} = require("../controllers/postController");
+const {index, show} = require("../controllers/postController");
 
 router.get("/", index);
+
+router.get("/:id", show)
 
 module.exports = router;
