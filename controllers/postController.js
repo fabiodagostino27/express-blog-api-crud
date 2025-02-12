2fd61a68dc099977f3c8054b5e16d27d94bdbbfd
fabiodagostino27 <fabiodago27@gmail.com>
2025-02-12 15:20:12 +0100

    ADD: destroy route

--- controllers/postController.js
+++ controllers/postController.js
@@ -22,6 +22,23 @@ const store = (req, res) => res.send("Creazione nuovo post");
 const update = (req, res) => res.send(`Modifica integrale del post: ${req.params.id}`);
 const modify = (req, res) => res.send(`Modifica parziale del post: ${req.params.id}`);
 
+const destroy = (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find(post => post.id === id);
+    
+    if (!post) {
+        res.status(404);
+
+        return res.json ({
+            error: "Not Found",
+            message: "Post non trovato"
+        })
+    };
+
+    posts.splice(posts.indexOf(post), 1);
+    res.sendStatus(204);
+    console.log(posts);
+}
 
 
-module.exports = {index, show, store, update, modify};
+module.exports = {index, show, store, update, modify, destroy};
--- routers/posts.js
+++ routers/posts.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {index, show, store, update, modify} = require("../controllers/postController");
+const {index, show, store, update, modify, destroy} = require("../controllers/postController");
 
 router.get("/", index);
 router.get("/:id", show);
 router.post("/", store);
 router.put("/:id", update);
 router.patch("/:id", modify);
+router.delete("/:id", destroy);
 
 module.exports = router;
