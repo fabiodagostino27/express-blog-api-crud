3d3566dc2dc26d0719c4eff759cd58130a969d44
fabiodagostino27 <fabiodago27@gmail.com>
2025-02-14 11:17:16 +0100

    ADD: middlewares

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const routerPosts = require("./routers/posts")
+const errorsHandler = require("./middlewares/errorsHandler");
+const notFound = require("./middlewares/notFound");
 
 app.use(express.static("public"));
 app.use(express.json());
+app.use(notFound);
+app.use(errorsHandler);
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
-
-app.use("/posts", routerPosts)
+app.use("/posts", routerPosts);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
